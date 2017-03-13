1b59c2d8f255b32a8cc0f939a19b86c19dae7709
Xavier Corredor Llano <xcorredorl@ideam.gov.co>
2017-03-13 16:35:36 -0500

    Added pyramids script

---
+++ pyramids.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+# -*- coding: utf-8 -*-
+#
+#  Copyright (C) 2017 Xavier Corredor Llano, SMBYC
+#  Email: xcorredorl at ideam.gov.co
+#
+# Compute the pyramids for input tif
+#
+
+# break with any error
+set -e
+
+function clean () {
+    gdal_translate $1 tmp.tif
+    rm $1
+    mv tmp.tif $1
+}
+
+function compression () {
+    gdal_translate -co COMPRESS=DEFLATE -co PREDICTOR=2 $1 tmp.tif
+    rm $1
+    mv tmp.tif $1
+}
+
+function pyramids () {
+    gdaladdo --config BIGTIFF_OVERVIEW YES $1 2 4 8 16 32 64 128 256 512
+}
+
+echo -e "\nCompute the pyramids for:$@\n"
+
+for FILE in "$@"
+do
+    echo "compression: $FILE"
+    compression $FILE
+    echo "compute the pyramids: $FILE"
+    pyramids $FILE
+done
+
+echo "DONE"
